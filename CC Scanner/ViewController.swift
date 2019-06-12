3ffd65c1f5ce4bf3315f0f4bf67dbaa3363a42f8
Barbara Vanaki <bvanaki@chewy.com>
2019-06-12 10:12:48 -0400

    Initial Commit

---
+++ CC Scanner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CC Scanner
+//
+//  Created by Barbara Vanaki on 6/12/19.
+//  Copyright © 2019 Barbara Vanaki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
